c315ebf7fa3645b49ae59969a308d8f7dcce0bb0
ShraddhaDange <shraddha.dange1996@gmail.com>
2018-03-26 11:25:12 +0530

    shraddha

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -28,7 +28,6 @@ import { TechDetailsViewComponent } from "./tech-details-view/tech-details-view.
 import { ExcelPerformViewComponent } from "./excel-perform-view/excel-perform-view.component";
 import { BankDetailsComponent } from './bank-details/bank-details.component';
 import { sponserviewComponent } from "./sponserview/sponserview.component";
-import { EmployeeviewComponent  } from "./employeeview/employeeview.component";
 import { ViewResultComponent } from "./view-result/view-result.component";
 import { EventResult } from "../models/eventResult.model";
 
@@ -66,7 +65,7 @@ const routes: Routes = [
   {path:'exelperformview/:id',component:ExcelPerformViewComponent},
   {path:'bankdetails/:id',component:BankDetailsComponent},
   {path: 'sponserview/:id', component:sponserviewComponent},
-  {path:'employeedetails/:id' , component: EmployeeviewComponent },
+  
   {path:'viewresult/:id/:name',component:EventResult}
 ];
 
